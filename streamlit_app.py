3d6d9b9ebaf0cb6add82326d6057f0263412b5d0
Kafele-Kiewit <kafele.williams+github@kiewit.com>
2025-09-10 07:44:37 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,6 +51,8 @@ if ingredients_list:
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         st.subheader(fruit_chosen + ' Nutrition Information')
         fruityvice_response = requests.get('https://fruityvice.com/apu/fruit/' + search_on)
+        st.write(fruityvice_response.json())
+        st.stop()
         fv_df = st.dataframe(data=fruityvice_response.json(), width='stretch')
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
