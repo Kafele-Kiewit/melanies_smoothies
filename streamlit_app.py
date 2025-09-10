d4ff009770386102dff8c67696932c5667d51531
Kafele-Kiewit <kafele.williams+github@kiewit.com>
2025-09-10 07:46:15 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,7 +51,7 @@ if ingredients_list:
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         st.subheader(fruit_chosen + ' Nutrition Information')
         fruityvice_response = requests.get('https://fruityvice.com/apu/fruit/' + search_on)
-        st.write(fruityvice_response.json())
+        st.write(fruityvice_response)
         st.stop()
         fv_df = st.dataframe(data=fruityvice_response.json(), width='stretch')
 
